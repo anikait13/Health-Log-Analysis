9cc51b833542396a49ba9a0ef2e86477b9ea86ec
BD <bd@BDs-MacBook-Air.local>
2024-01-31 14:04:57 +0530

    anti plagarrism measures

--- Dashboard.py
+++ Dashboard.py
@@ -26,3 +26,8 @@ st.subheader("Important Notes 📌")
 st.markdown("- Certain necessary assumptions were considered during the calculation of analytics.")
 st.markdown("- All graphs and charts are interactive, allowing for further exploration.")
 st.markdown("- Source Code is seen by clicking the Github logo on the top-right ↗️. ")
+
+st.warning("The repository was plagarized by others as it was set to public for deployment.")
+st.write("Repository is set to private now. Source code will be sent as a .zip file")
+st.image("images/Clones.png")
+
